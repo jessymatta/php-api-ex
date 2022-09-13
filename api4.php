be1bf4f568cbe3d6963bc8b7a1eee8a37cc99058
jessymatta <jessy.w.matta@gmail.com>
2022-09-13 03:56:06 +0300

    modified api4 to return a JSON object

--- api4.php
+++ api4.php
@@ -5,6 +5,7 @@ $curr_date_in_sec=strtotime($current_date);
 $target_date = strtotime("2022-12-25");
 $diff_in_seconds = $target_date-$curr_date_in_sec;
 $diff_in_days = round($diff_in_seconds / (60 * 60 * 24));
-echo"$diff_in_days"." days left";
+$result=["days_left"=>$diff_in_days];
+echo json_encode($result);
 
 ?>
