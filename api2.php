4882e0b84189599edff0ff36726484e5fcafdd79
jessymatta <jessy.w.matta@gmail.com>
2022-09-13 03:10:46 +0300

    modified api2

--- api2.php
+++ api2.php
@@ -1,20 +1,10 @@
 <?php
 
-// Precondition b: should be different from 0
-function expression($a,$b,$c) {
-    if($b!=0){
-    return $a**3+$b*$c-$a/$b;
-    }
-}
-
-
 if( isset ($_POST["a"],$_POST["b"],$_POST["c"] ) ){
     $a=$_POST["a"];
     $b=$_POST["b"];
     $c=$_POST["c"];
-    echo "expression($a,$b,$c)";
-    
+    $output=$a**3+$b*$c-$a/$b;
+    echo "$output";
     }
-
-
 ?>
